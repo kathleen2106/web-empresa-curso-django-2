a32e1e1e50ffba03bef123eaea726a3f1c335e28
Kathleen Gonzales <besai.gp123@gmail.com>
2023-03-07 10:33:20 -0500

    Añadido proyecto

---
+++ webpersonal/portafolio/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class PortafolioConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'portafolio'
+    verbose_name = 'portafolio'
+    
